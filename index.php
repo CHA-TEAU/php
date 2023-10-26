092d7085d289306b9fdf789aa5cdae43350b5530
CH4TE4U <136450506+CHA-TEAU@users.noreply.github.com>
2023-10-26 21:11:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -53,64 +53,139 @@
 
 <?php
 
-$db_address = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_testdb = "test DB";
+// $db_address = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_testdb = "test DB";
 
 
 
 
-$db = @new mysqli($db_address, $db_user, $db_pass, $db_testdb);
+// $db = @new mysqli($db_address, $db_user, $db_pass, $db_testdb);
 
-if(isset($_POST['submit'])){
+// if(isset($_POST['submit'])){
     
-    if($db -> connection_errno)
+//     if($db -> connection_errno)
+//     {
+//         echo "Error occured!" . $db -> connection_errno;
+//     }else
+//     {
+//         $anim = $_POST['new_animal'];
+//         $query = $db -> query("INSERT INTO `myFzoo`(`animal`) VALUES ('$anim')");
+//     }
+// }
+
+// if(isset($_POST['del'])){
+
+  
+//         $anim = $_POST['new_animal'];
+//         $query = $db -> query("DELETE FROM `myFzoo` WHERE `animal` = ('$anim')");
+    
+// }
+
+
+
+// if($db -> connection_errno)
+// {
+//     echo "Error occured!" . $db -> connection_errno;
+// }else
+// {
+//     $query = $db -> query("SELECT * FROM `myFzoo`");
+//     // $result = $query -> fetch_assoc();
+//     // var_dump($result);
+
+//     echo "<table border = 1>";
+//     while($row = $query -> fetch_assoc()){
+
+//         echo "<tr>";
+//         echo "<td>";
+//         echo $row['id'];
+//         echo "</td>";
+
+//         echo "<td>";
+//         echo $row['animal'];
+//         echo "</td>";
+//         echo "</tr>";
+//     }
+
+//     echo "</table>";
+// }
+?>
+
+<?php
+
+include 'DBCrud.php';
+$dbcrud = new DBCrud('zoo', 'localhost', 'root', '', 'testdb');
+
+
+$arr = [
+    "Animal" => "Frog"
+];
+
+
+$dbcrud->Insert($arr);
+
+$dbcrud->Delete('animal', "Собака");
+
+$dt = $dbcrud->Select(["id", "animal"]);
+echo '<pre>';
+var_dump($dt);
+echo '</pre>';
+$db = @new mysqli($db_address,$db_user, $db_pass,$db_testdb);
+if(isset($_POST['submit']))
+{
+    if($db->connection_errno)
     {
-        echo "Error occured!" . $db -> connection_errno;
+        echo "error: ".$db->connection_errno;
     }else
     {
         $anim = $_POST['new_animal'];
-        $query = $db -> query("INSERT INTO `myFzoo`(`animal`) VALUES ('$anim')");
+        $query = $db->query("INSERT INTO `zoo`(`animal`) VALUES ('$anim')");
     }
 }
 
-if(isset($_POST['del'])){
-
-  
+if(isset($_POST['delete']))
+{
+    if($db->connection_errno)
+    {
+        echo "error: ".$db->connection_errno;
+    }else
+    {
         $anim = $_POST['new_animal'];
-        $query = $db -> query("DELETE FROM `myFzoo` WHERE `animal` = ('$anim')");
-    
+        $query = $db->query("DELETE FROM `zoo` WHERE `animal` = '$anim';");
+    }
 }
 
 
 
-if($db -> connection_errno)
+if($db->connection_errno)
 {
-    echo "Error occured!" . $db -> connection_errno;
-}else
-{
-    $query = $db -> query("SELECT * FROM `myFzoo`");
-    // $result = $query -> fetch_assoc();
-    // var_dump($result);
+    echo "error: ".$db->connection_errno;
+}else{
+    $query = $db->query("SELECT * FROM `zoo`");
+    //$res = $query->fetch_assoc();
+    echo '<table border="1">';
+    while($row = $query->fetch_assoc())
+    {
+        echo '<tr>';
 
-    echo "<table border = 1>";
-    while($row = $query -> fetch_assoc()){
-
-        echo "<tr>";
-        echo "<td>";
+        echo '<td>';
         echo $row['id'];
-        echo "</td>";
+        echo '</td>';
 
-        echo "<td>";
+
+        echo '<td>';
         echo $row['animal'];
-        echo "</td>";
-        echo "</tr>";
-    }
+        echo '</td>';
 
-    echo "</table>";
+        echo '</tr>';
+        
+    }
+    echo '</table>';
+    //var_dump($res);
 }
 ?>
+
 
 <form method="post">
 
